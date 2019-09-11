2645aeb29b31128a329b65113ba1cc7a4ef7243d
tonecodez <tonydinhh@gmail.com>
2019-09-11 15:31:37 -0700

    pushing src code

---
+++ src/prog1_client.c
@@ -0,0 +1,14 @@
+/* Assignment 1
+ * Computer Networks, Winter 2019
+ * Tanzima Islam
+ *
+ * Tony Dinh, Anders Bergquist
+ *
+ */
+
+int main(int argc, char* argv[]) {
+
+   printf("Client side for program assignment 1");
+
+   return 0; 
+}
